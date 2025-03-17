fc3e9e71610ce8b13ba0d9ea685a7908cad00921
Luqtta <lorenbrryt@gmail.com>
2025-03-16 22:48:47 -0300

    Update traducao.js

--- src/javascript/traducao.js
+++ src/javascript/traducao.js
@@ -7,11 +7,11 @@ function loadLanguageFile() {
     const language = getUserLanguage().toLowerCase();
 
     if (language.startsWith('en')) {
-        window.location.href = './en.html'; 
+        window.location.href = '/en';
     } else if (language.startsWith('pt-br')) {
-        window.location.href = './pt-br.html';  
+        window.location.href = '/pt-br';
     } else {
-        window.location.href = './en.html';  
+        window.location.href = '/en';
     }
 }
 
